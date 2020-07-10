2e3d058a5e26ca3ef23a4f36160b94e87726f694
Griffin Schulert <griffin.bruce@gmail.com>
2020-07-10 16:31:21 -0400

    Small Error Fix

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -19,7 +19,7 @@ campsiteRouter.route('/')
 .post((req, res, next) => {
     Campsite.create(req.body)
     .then(campsite => {
-        console.log('Campsite created ', campsite);
+        console.log('Campsite Created ', campsite);
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
         res.json(campsite);
