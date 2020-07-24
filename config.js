6af889e1400960de25da31093ac39aed3b961840
Griffin Schulert <griffin.bruce@gmail.com>
2020-07-24 16:00:03 -0400

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '219653299250181',
+        clientSecret: '277668c61a268efcc7103c11eea9d0fc'
+    }
 }
